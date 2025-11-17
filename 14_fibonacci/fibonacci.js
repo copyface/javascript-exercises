8e31b4da680693de60cfd6bdafe7e67514a9ed11
copyface <kenthendico011@gmail.com>
2025-11-17 19:13:22 +0800

    feat: solve fibonacci
    copied for now cause i dont understand

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,24 @@
-const fibonacci = function (num) {
-  let ans = 0;
-  for (let i = 1; i === num; i++) {}
+const fibonacci = function (countArg) {
+  let count;
+  if (typeof countArg !== "number") {
+    count = parseInt(countArg);
+  } else {
+    count = countArg;
+  }
+
+  if (count < 0) return "OOPS";
+  if (count == 0) return 0;
+
+  let firstPrev = 1;
+  let secondPrev = 0;
+
+  for (let i = 2; i <= count; i++) {
+    let current = firstPrev + secondPrev;
+    secondPrev = firstPrev;
+    firstPrev = current;
+  }
+
+  return firstPrev;
 };
 
 // Do not edit below this line
